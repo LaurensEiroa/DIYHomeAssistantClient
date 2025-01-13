cc76605d5df982b0f93075b44fab28ebf1442b5c
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-13 14:37:09 +0100

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import asyncio
 # Example usage
 if __name__ == "__main__":
-    from scr.utils.picamera.picamera import test_camera_http_streamer
-    asyncio.run(test_camera_http_streamer())
+    #from scr.utils.picamera.picamera import test_camera_http_streamer
+    #asyncio.run(test_camera_http_streamer())
+
+    from scr.websocket.websocket_client import Client
+    cli = Client()
+    asyncio.run(cli.start_server())
