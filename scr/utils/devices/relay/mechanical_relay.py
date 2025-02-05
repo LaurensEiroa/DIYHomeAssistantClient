359567c209ae5d88a1e9acfac1f4c8bf1ba06f89
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-02-05 15:01:46 +0100

    .

--- scr/utils/devices/relay/mechanical_relay.py
+++ scr/utils/devices/relay/mechanical_relay.py
@@ -5,23 +5,30 @@ class Relay:
     def __init__(self,GPIO_PIN=26,test=False):
         # Create a relay object
         self.relay = gpiozero.OutputDevice(GPIO_PIN, active_high=True, initial_value=False)
+        self.relay_status = False
         #self.turn_on_relay()
 
     def turn_on_relay(self):
         print("Turning on the relay")
+        self.relay_status = True
         self.relay.on()
 
     def turn_off_relay(self):
         print("Turning off the relay")
+        self.relay_status = False
         self.relay.off()
 
     def toggle_relay(self):
         print("Toggling the relay")
+        self.relay_status = not self.relay_status
         self.relay.toggle()
 
     def close_relay(self):
         print("Closing the relay")
         self.relay.close() # release the GPIO pin
+
+    def get_relay_status(self):
+        return self.relay_status
 
     def test_relay(self):
         print("Testing the relay")
